e9a430df7807f2302ece22cd11c49603aa9adb9a
ryanjnam99 <ryanjnam@yahoo.com>
2023-07-29 16:48:29 -0700

    first commit

---
+++ Ryan/Intro.java
@@ -0,0 +1,7 @@
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("My name is Ryan Nam");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometwon is Clyde Hill");
+    }
+}
