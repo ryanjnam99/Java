1b3a5f53a8faa52ff2fc4c4c6f6039df9b4ff1c7
ryanjnam99 <ryanjnam@yahoo.com>
2023-07-30 22:59:37 -0700

    finished cafe business

--- Sample/Sample.java
+++ Sample/Sample.java
@@ -1,34 +1,20 @@
-
+import java.util.ArrayList;
 
 public class Sample {
     public static void main(String[] args) {
-        int day = 2;
-        switch(day){
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("There are only seven days in a week...");
+        ArrayList<String> snacks = new ArrayList<String>();
+        snacks.add("Apples");
+        snacks.add("Almonds");
+        snacks.add("Pretzels");
+        snacks.add("Yogurt");
+
+        // Use reverse for loop to safely remove elements
+        for(int i=0; i<snacks.size(); i++) {
+            if(snacks.get(i).charAt(0) == 'A') {
+                snacks.remove(i);
+                i--; // Here we move one step back
+            }
         }
-
-
+        System.out.println(snacks);
     }
 }
