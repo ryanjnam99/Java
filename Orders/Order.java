1706a9abf88b24e053ff9d60377b6457ba80d039
ryanjnam99 <ryanjnam@yahoo.com>
2023-08-01 14:32:59 -0700

    finished Barista

--- Orders/Order.java
+++ Orders/Order.java
@@ -1,7 +1,86 @@
 import java.util.ArrayList;
-class Order {
-    public String name;
-    public double total;
-    public boolean ready; 
-    public ArrayList<Item> items = new ArrayList<>();
+    
+// Here we're creating a new data type called Order
+public class Order {
+    
+    // MEMBER VARIABLES
+    private String name; // default value of null
+    private boolean ready; // default value false
+    private ArrayList<Item> items; // defaults to null
+    
+    // CONSTRUCTOR
+    // No arguments, sets name to "Guest", initializes items as an empty list.
+    public Order() {
+        this.name = "Guest";
+        this.items = new ArrayList<Item>();
+    }
+    
+    // OVERLOADED CONSTRUCTOR
+    // Takes a name as an argument, sets name to this custom name.
+    // Initializes items as an empty list.
+    public Order(String name) {
+        this.name = name;
+        this.items = new ArrayList<Item>();
+    }
+
+    
+    // ORDER METHODS
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public String getStatusMessage() {
+        if (this.ready) {
+            return "Your order is ready.";
+        } else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal() {
+        double total = 0;
+        for (Item item: items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public void display()  {
+        System.out.println("Customer Name:" + " " + this.name);
+        for (Item item: items) {
+            System.out.println(item.getName() + " -$" + item.getPrice());
+        }
+
+    }
+
+    
+    	// Most of your code will go here, 
+    	// so implement the getters and setters first!
+    
+    // GETTERS & SETTERS
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getReady() {
+        return this.ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+    
+    public ArrayList<Item> getItems() {
+        return this.items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
+
--- Sample/Car.java
+++
@@ -1,7 +0,0 @@
-class Car {
-    public int year;
-    public String make;
-    public String model;
-    public String color;
-}
-
